f9c77c0764c49ace0bea26ed0be3009e33914272
Kamila Benevides <kab@ic.ufal.br>
2023-10-02 09:51:01 -0300

    add front-end

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container class="fill-height justify-center">
+    <v-responsive class="align-center text-center fill-height">
+
+      <v-sheet :height="700" :width="1200" color="pink-lighten-4">
+        <v-row>
+          <v-col cols="6">
+            <v-col class="mx-auto pl-14 pt-10 pb-8" theme="dark" max-width="400">
+              <h1 class="title text-h2 text-left font-weight-bold">Descubra as jóias escondidas da literatura</h1>
+            </v-col>
+            <v-col class="mx-auto px-14" theme="dark" max-width="400">
+              <p class="text text-h5 text-left">
+                Desbloqueie o poder dos livros com o Book Graph. A nossa plataforma utiliza algoritmos avançados para
+                extrair informações valiosas dos livros.
+              </p>
+            </v-col>
+            <v-col cols="auto pa-10">
+              <v-row justify="center" class="d-flex">
+                <v-col>
+                  <v-autocomplete rounded variant="solo-filled" label="Busque o livro"
+                    placeholder="Rainha Vermelha" v-model="values" :items="items"></v-autocomplete>
+                </v-col>
+                <v-col cols="auto">
+                  <v-btn rounded variant="elevated" class="dark" size="x-large" color="purple-darken-4"
+                    prepend-icon="mdi-magnify" @click="goTo()">Buscar</v-btn>
+                </v-col>
+
+              </v-row>
+            </v-col>
+          </v-col>
+
+          <v-col cols="6">
+            <img src="../assets/girl_book.jpg">
+          </v-col>
+        </v-row>
+      </v-sheet>
+
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: ['Rainha Vermelha', 'Harry Potter'],
+      values: '',
+    }),
+    methods: {
+      goTo() {
+        this.$router.push('/book')
+      }
+    }
+  }
+</script>
+
+<style>
+.title{
+  font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji !important;
+  line-height: 1.4;
+}
+.text{
+  font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji !important;
+}
+
+
+</style>
---
+++ src/views/bookPage.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-parallax src="../assets/capa.jpg">
+        <div class="d-flex flex-column fill-height justify-center align-center text-white">
+            <v-sheet height="100" :elevation="20" color="grey-lighten-2" rounded>
+                <h1 class="text-h1 font-weight-thin mb-4">
+                    Rainha Vermelha
+                </h1>
+            </v-sheet>
+        </div>
+    </v-parallax>
+
+    <v-container>
+        <div class="py-15">
+            <p class="text-snopse">Sinopse</p>
+            <p class="text"> O mundo de Mare Barrow é dividido pelo sangue: vermelho ou prateado. Mare e sua família são vermelhos: plebeus, humildes, destinados a servir uma elite prateada cujos poderes sobrenaturais os tornam quase deuses. Mare rouba o que pode para ajudar sua família a sobreviver e não tem esperanças de escapar do vilarejo miserável onde mora. Entretanto, numa reviravolta do destino, ela consegue um emprego no palácio real, onde, em frente ao rei e a toda a nobreza, descobre que tem um poder misterioso... Mas como isso seria possível, se seu sangue é vermelho? Em meio às intrigas dos nobres prateados, as ações da garota vão desencadear uma dança violenta e fatal, que colocará príncipe contra príncipe - e Mare contra seu próprio coração.</p>
+        </div>
+    </v-container>
+    
+  <v-container class="pt-10">
+      <div>
+        <p class="text-snopse pb-5">Perguntas</p>
+        <v-text-field
+          variant="outlined"
+          label="Faça uma pergunta sobre o livro"
+          disabled=""
+          ></v-text-field>
+        <v-expansion-panels class="mb-6">
+          <v-expansion-panel v-for="i in pokemons" :key="i">
+            <v-expansion-panel-title expand-icon="mdi-menu-down">
+              {{ i.question }} 
+            </v-expansion-panel-title>
+            <v-expansion-panel-text v-for="j in i.answer" :key="j"
+              >
+              {{ j }}
+              </v-expansion-panel-text
+            >
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
+
+  </v-container>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data: () => ({
+      questions: [],
+      answer: [],
+      pokemons: []
+    }),
+    created: 
+        function(){
+            axios.get("http://localhost:5000/questions").then(res =>{
+            this.pokemons = res.data;
+            })
+        }
+    }
+</script>
+
+<style>
+.text-snopse{
+  font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji !important;
+  font-size: 5rem;
+}
+</style>
